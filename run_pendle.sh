bbf472446b9dd1275aff9564d2d17e4f51d9abf7
halfdoctor <nmanura@gmail.com>
2025-11-15 16:26:49 +0000

    feat: Update .gitignore and add run_pendle.sh script for environment setup

---
+++ run_pendle.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# python3 -m venv ./venv
+# source ./venv/bin/activate
+# pip install -r requirements.txt
+
+cd /home/nemin/pendle
+source venv/bin/activate
+python -u pendle_market_analysis_optimized.py 2>&1 | awk '{print strftime("%Y-%m-%d %H:%M:%S"), $0}' >> pendle.log
